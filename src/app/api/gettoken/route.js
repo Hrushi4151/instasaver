5078b98bec91e7156ef425d348e2aa20178829e4
Hrushi4151 <108774635+Hrushi4151@users.noreply.github.com>
2025-04-27 13:38:44 +0530

    Update route.js

--- src/app/api/gettoken/route.js
+++ src/app/api/gettoken/route.js
@@ -1,53 +1,56 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 
-// export async function POST(req,res){
-//   const body=await req.json();
-//   let url=body.url;
-//   console.log(url)
-//         const myres = await fetch("https://v3.savevid.net/api/ajaxSearch", {
-//           credentials: 'include',
-//                   mode: 'no-cors',
-//                   method: "POST",
-//                   headers: {
-//                     Accept: "*/*",
-//                     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-//                     "ACCESS-CONTROL-ALLOW-ORIGIN": "*" ,
-//                     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-//                     "Origin":"https://saveig.app",
-//                     "Referer":"https://saveig.app/"
-//           },
-//           body: `q=${url}&t=media&lang=en`,
-//         });
-//         const response = await myres.json();
-//         console.log(response);
+export async function POST(req,res){
+  const body=await req.json();
+  let url=body.url;
+  console.log(url)
+        const myres = await fetch("https://api.instasave.website/media", {
+          credentials: 'include',
+                  mode: 'no-cors',
+                  method: "POST",
+                  headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                    "ACCESS-CONTROL-ALLOW-ORIGIN": "*" ,
+                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "Origin":"https://saveig.app",
+                    "Referer":"https://saveig.app/"
+          },
+          body: `q=${url}&lang=en`,
+        });
+        const response = await myres.json();
+        console.log(response);
           
-//       return NextResponse.json({"status":200,response});
+      return NextResponse.json({"status":200,response});
 
-// }
+}
 
-import { NextResponse } from 'next/server';
 
-export async function POST(req) {
-  try {
-    const body = await req.json();
-    const url = body.url;
 
-    const formData = new FormData();
-    formData.append('q', url);
-    formData.append('lang', 'en');
 
-    const myres = await fetch("https://api.instasave.website/media", {
-      method: "POST",
-      headers: {
-        "Accept": "*/*",
-        "Origin": "https://saveig.app",
-        "Referer": "https://saveig.app/"
-      },
-      body:  `url=${url}&lang=en`,
-    });
+// import { NextResponse } from 'next/server';
 
-    const response = await myres.json();
+// export async function POST(req) {
+//   try {
+//     const body = await req.json();
+//     const url = body.url;
+
+//     const formData = new FormData();
+//     formData.append('q', url);
+//     formData.append('lang', 'en');
+
+//     const myres = await fetch("https://api.instasave.website/media", {
+//       method: "POST",
+//       headers: {
+//         "Accept": "*/*",
+//         "Origin": "https://saveig.app",
+//         "Referer": "https://saveig.app/"
+//       },
+//       body:  `url=${url}&lang=en`,
+//     });
+
+//     const response = await myres.json();
 
     // const htmlContent = data.data;
 
@@ -62,12 +65,12 @@ export async function POST(req) {
     //   return NextResponse.json({ error: "Failed to extract thumbnail or download URL." }, { status: 400 });
     // }
 
-    console.log(response);
+//     console.log(response);
           
- return NextResponse.json({"status":200,"response":response});
+//  return NextResponse.json({"status":200,"response":response});
 
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500,error });
-  }
-}
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: "Internal server error" }, { status: 500,error });
+//   }
+// }
